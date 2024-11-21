41bf02fe48cfa5d2251c68d05cb6c9dd17e91770
rna-afk <anarayan@redhat.com>
2024-11-21 11:56:31 -0500

    Added unit tests for ignition config with the option to test for specific ignition files and their contents.

--- pkg/installer/custominstallconfig_test.go
+++ pkg/installer/custominstallconfig_test.go
@@ -4,8 +4,11 @@ package installer
 // Licensed under the Apache License 2.0.
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/profiles/2018-03-01/resources/mgmt/resources"
@@ -19,6 +22,7 @@ import (
 	icazure "github.com/openshift/installer/pkg/asset/installconfig/azure"
 	"github.com/openshift/installer/pkg/asset/installconfig/azure/mock"
 	"github.com/openshift/installer/pkg/asset/releaseimage"
+	"github.com/openshift/installer/pkg/asset/targets"
 	"github.com/openshift/installer/pkg/ipnet"
 	"github.com/openshift/installer/pkg/types"
 	azuretypes "github.com/openshift/installer/pkg/types/azure"
@@ -276,8 +280,6 @@ func mockClientCalls(client *mock.MockAPI) {
 			ID:       to.StringPtr("test-resource-group"),
 			Location: to.StringPtr("centralus"),
 		}, nil)
-	client.EXPECT().ListResourceIDsByGroup(gomock.Any(), "test-resource-group").
-		Return([]string{}, nil)
 	client.EXPECT().GetHyperVGenerationVersion(gomock.Any(), "Standard_D2s_v3", "centralus", "").
 		Return("V2", nil)
 }
@@ -303,27 +305,28 @@ func TestApplyInstallConfigCustomisations(t *testing.T) {
 	t.Log(bootstrapIgnition)
 }
 
-func TestApplyIgnitionConfigCustomisations(t *testing.T) {
+func TestApplyManifestsCustomization(t *testing.T) {
 	m := fakeManager()
 	inInstallConfig := makeInstallConfig()
-
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
-	mockClient := azuremock.NewMockAPI(mockCtrl)
+	mockClient := mock.NewMockAPI(mockCtrl)
 	inInstallConfig.Azure.UseMockClient(mockClient)
 	mockClientCalls(mockClient)
 
-	graph, err := m.applyInstallConfigCustomisations(inInstallConfig, makeImage())
+	g, err := m.applyInstallConfigCustomisations(inInstallConfig, makeImage())
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	graph, err = m.applyIgnitionConfigCustomisations(graph)
-	if err != nil {
-		t.Fatal(err)
+	for _, a := range targets.Manifests {
+		err := g.Resolve(a)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 
-	bootstrapAsset := graph.Get(&bootstrap.Bootstrap{}).(*bootstrap.Bootstrap)
+	bootstrapAsset := g.Get(&bootstrap.Bootstrap{}).(*bootstrap.Bootstrap)
 	var temp map[string]any
 	err = json.Unmarshal(bootstrapAsset.Files()[0].Data, &temp)
 	if err != nil {
@@ -331,25 +334,89 @@ func TestApplyIgnitionConfigCustomisations(t *testing.T) {
 	}
 
 	verifyIgnitionFiles(t, temp, expectedBootstrapStorageFileList, expectedBootstrapSystemdFileList, bootstrapAsset.Files()[0].Filename)
+
+	// file, err := json.MarshalIndent(g, "", " ")
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// os.WriteFile("test1.json", file, 0777)
 }
+
+// func TestApplyIgnitionConfigCustomisations(t *testing.T) {
+// 	m := fakeManager()
+// 	inInstallConfig := makeInstallConfig()
+// 	mockCtrl := gomock.NewController(t)
+// 	defer mockCtrl.Finish()
+// 	mockClient := azuremock.NewMockAPI(mockCtrl)
+// 	inInstallConfig.Azure.UseMockClient(mockClient)
+// 	mockClientCalls(mockClient)
+
+// 	graph, err := m.applyInstallConfigCustomisations(inInstallConfig, makeImage())
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	graph, err = m.applyIgnitionConfigCustomisations(graph)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	// file, err := json.MarshalIndent(temp, "", " ")
+// 	// if err != nil {
+// 	// 	t.Fatal(err)
+// 	// }
+// 	// os.WriteFile("test.json", file, 0777)
+// }
 
 func verifyIgnitionFiles(t *testing.T, temp map[string]any, storageFiles []string, systemdFiles []string, fileName string) {
 	files := (temp["storage"].(map[string]any))["files"].([]any)
 	systemd := (temp["systemd"].(map[string]any))["units"].([]any)
-	storageFileList := map[string]bool{}
+	storageFileList := map[string]string{}
 	for _, file := range files {
-		storageFileList[file.(map[string]any)["path"].(string)] = true
+		contents, found := file.(map[string]any)["contents"]
+		if !found {
+			contents = file.(map[string]any)["append"].([]any)[0]
+		}
+		storageFileList[file.(map[string]any)["path"].(string)] = contents.(map[string]any)["source"].(string)
 	}
-	systemdFileList := map[string]bool{}
+	systemdFileList := map[string]string{}
 	for _, file := range systemd {
-		systemdFileList[file.(map[string]any)["name"].(string)] = true
+		contents, found := file.(map[string]any)["contents"]
+		if !found {
+			contents = file.(map[string]any)["dropins"].([]any)[0].(map[string]any)["contents"]
+		}
+		systemdFileList[file.(map[string]any)["name"].(string)] = contents.(string)
 	}
-
+	// tempValues := map[string]string{}
 	for _, file := range storageFiles {
-		assert.True(t, systemdFileList[file], fmt.Sprintf("file %v missing in storage file list in ignition file %s", file, fileName))
+		content, isFound := storageFileList[file]
+		assert.True(t, isFound, fmt.Sprintf("file %v missing in storage file list in ignition file %s", file, fileName))
+		if isFound {
+			fileContents, err := base64.StdEncoding.DecodeString(strings.Split(content, "base64")[1][1:])
+			if err != nil {
+				t.Fatal(err)
+			}
+			if file == "/opt/openshift/manifests/aro-imageregistry.yaml" {
+				content := string(fileContents)
+				re := regexp.MustCompile(`httpSecret: "[A-Za-z0-9]+"`)
+				fileContents = []byte(re.ReplaceAllString(content, `httpSecret: "test"`))
+			}
+			assert.EqualValues(t, Storage[file], string(fileContents), fmt.Sprintf("missing storage data in file %v", file))
+		}
 	}
-
+	// con, _ := json.MarshalIndent(tempValues, "", " ")
+	// os.WriteFile("storage_files_default.go", con, 0777)
+	// tempValues = map[string]string{}
 	for _, file := range systemdFiles {
-		assert.True(t, storageFileList[file], fmt.Sprintf("file %v missing from systemd file list in ignition file %s", file, fileName))
+		content, isFound := systemdFileList[file]
+		assert.True(t, isFound, fmt.Sprintf("file %v missing from systemd file list in ignition file %s", file, fileName))
+		if isFound {
+			// tempValues[file] = content
+			assert.EqualValues(t, Systemd[file], content, fmt.Sprintf("missing systemd data in file %v", file))
+		}
 	}
+	// con, _ = json.MarshalIndent(tempValues, "", " ")
+	// os.WriteFile("systemd_files_default.go", con, 0777)
+
+	// Test file contents.
+
 }
