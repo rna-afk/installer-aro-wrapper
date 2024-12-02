d914e29a2193d4123c6e3f2deee9e283b2243835
rna-afk <anarayan@redhat.com>
2024-12-02 16:05:58 -0500

    aro: Added manifests files
    Adding manifests files to the asset.

--- pkg/installer/custominstallconfig.go
+++ pkg/installer/custominstallconfig.go
@@ -25,6 +25,7 @@ import (
 	"github.com/openshift/installer-aro-wrapper/pkg/cluster/graph"
 	"github.com/openshift/installer-aro-wrapper/pkg/installer/dnsmasq"
 	"github.com/openshift/installer-aro-wrapper/pkg/installer/etchost"
+	"github.com/openshift/installer-aro-wrapper/pkg/installer/manifests"
 	"github.com/openshift/installer-aro-wrapper/pkg/installer/mdsd"
 )
 
@@ -132,6 +133,19 @@ func (m *manager) applyInstallConfigCustomisations(installConfig *installconfig.
 		return nil, err
 	}
 	err = etchost.AppendEtcHostFiles(bootstrapAsset, *installConfig, localdnsConfig)
+	if err != nil {
+		return nil, err
+	}
+	config := manifests.ManifestsConfig{
+		AROWorkerRegistries: manifests.AroWorkerRegistries(installConfig.Config.ImageDigestSources),
+		HTTPSecret:          imageRegistryConfig.HTTPSecret,
+		AccountName:         imageRegistryConfig.AccountName,
+		ContainerName:       imageRegistryConfig.ContainerName,
+		CloudName:           installConfig.Config.Azure.CloudName.Name(),
+		AROIngressInternal:  installConfig.Config.Publish == "Internal",
+		AROIngressIP:        dnsConfig.IngressIP,
+	}
+	err = manifests.AppendManifestsFilesToBootstrap(bootstrapAsset, config)
 	if err != nil {
 		return nil, err
 	}
